04f748e65ce7d1ecd9db8d77445e6a327d9c667f
Alex <rubanovv.alex@gmail.com>
2022-11-01 14:59:12 +0100

    Tabs class toggling and adding content on page

---
+++ js/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsParent = document.querySelector('.tabheader__items'),
+          tabsContent = document.querySelectorAll('.tabcontent');
+
+    // Hide Content
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide', 'fade_out');
+            item.classList.remove('show');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    // Show Content and change active class
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade_in');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+
+        
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    // Switching content by click on tab
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item , i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
+
