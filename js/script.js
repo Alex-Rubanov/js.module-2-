52669939f35715bfb074f5af1e0c97bd7af75995
Alex <rubanovv.alex@gmail.com>
2022-11-03 04:01:00 +0100

    added modification of modal / by using setTimeout and by scroll

--- js/script.js
+++ js/script.js
@@ -117,17 +117,18 @@ window.addEventListener('DOMContentLoaded', () => {
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-close]');
 
+    function openModal() {
+        modal.classList.add('show');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            // modal.classList.toggle('show'); Alternative method
-            document.body.style.overflow = 'hidden';
-        });       
+        btn.addEventListener('click', openModal);       
     });
 
     function closeModal() {
         modal.classList.remove('show');
-        // modal.classList.toggle('show'); Alternative method
         document.body.style.overflow = '';
     }
 
@@ -144,4 +145,16 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     });
+
+    const modalTimerId = setTimeout(openModal, 4000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
